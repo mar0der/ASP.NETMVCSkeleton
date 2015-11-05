3eab950c7da6b394a5ee263a5ad836d549402430
Petar Petkov <petar.b.petkov@gmail.com>
2015-11-05 23:55:22 +0400

    Random generator added in common

---
+++ Exam.GlobalConstants/Contracts/IRandomGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace Exam.Common.Contracts
+{
+    using System;
+
+    public interface IRandomGenerator
+    {
+        string RandomString(int minLength = 5, int maxLength = 50);
+
+        bool RandomBool();
+
+        DateTime RandomDate(bool fature = true);
+
+        int RandomNumber(int max);
+    }
+}
---
+++ Exam.GlobalConstants/RandomGenerator.cs
@@ -0,0 +1,67 @@
+﻿namespace Exam.Common
+{
+    #region
+
+    using System;
+    using System.Text;
+
+    using Exam.Common.Contracts;
+
+    #endregion
+
+    public class RandomGenerator : IRandomGenerator
+    {
+        private const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz           !?.";
+
+        private readonly Random random;
+
+        public RandomGenerator()
+        {
+            this.random = new Random();
+        }
+
+        public string RandomString(int minLength = 5, int maxLength = 50)
+        {
+            var result = new StringBuilder();
+            var length = this.random.Next(minLength, maxLength);
+
+            for (var i = 0; i < length; i++)
+            {
+                result.Append(Letters[this.random.Next(Letters.Length)]);
+            }
+            return result.ToString();
+        }
+
+        public bool RandomBool()
+        {
+            var result = this.random.Next(2);
+            if (result == 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public DateTime RandomDate(bool fature = true)
+        {
+            var multiplier = 1;
+
+            var resultDate = DateTime.Now;
+            if (fature == false)
+            {
+                multiplier = -1;
+            }
+
+            resultDate = resultDate.AddDays(multiplier * this.random.Next(356)).AddHours(multiplier * this.random.Next(24)).AddMinutes(multiplier * this.random.Next(59));
+
+            return resultDate;
+
+        }
+
+        public int RandomNumber(int max)
+        {
+            return this.random.Next(max);
+        }
+    }
+}
